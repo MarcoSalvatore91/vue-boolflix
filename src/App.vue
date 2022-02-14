f53668143a4e26c3380f81088a4b6268155eb86d
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-14 13:11:16 +0100

    added axios in app

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,38 @@ import Main from "./components/Main.vue";
 
 export default {
   name: "App",
+
+  data() {
+    return {
+      api_key: "0fe42dceece4f3ca88e1c2b3123892c7",
+      query: "",
+    }
+  },
+
   components: {
     Header,
     Main,
   },
+
+  methods: {
+    fetchMovie() {
+
+      const config = {
+        params: {
+          api_key: this.api_key,
+          query: this.query,
+          language: 'it-IT'
+        }
+      }
+
+      axios.get("https://api.themoviedb.org/3/search/movies", config).then((res) => {
+        this.movies = res.data.results;
+      })
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-  
+
 </style>
